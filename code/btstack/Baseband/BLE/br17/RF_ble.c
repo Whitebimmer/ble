feb0c36559c3bf6f5f1ff1699fe3cccb612c2bc1
Tsai1005 <change.tsai@gmail.com>
2016-12-06 17:38:35 +0800

    1.fix NACK latency;

--- code/btstack/Baseband/BLE/br17/RF_ble.c
+++ code/btstack/Baseband/BLE/br17/RF_ble.c
@@ -229,6 +229,25 @@ static void __set_hw_state(struct ble_hw *hw, int state,
 	BLE_ANCHOR_CON0 = (2<<12)|(HW_ID(hw)<<8)|(1<<2)|1;
 }
 
+static int __pop_latency(struct ble_hw *hw)
+{
+	BLE_ANCHOR_CON0 = (2<<12) | (HW_ID(hw)<<8) | BIT(1);
+	return BLE_ANCHOR_CON2;
+}
+
+static void __push_latency(struct ble_hw *hw, int latency)
+{
+	BLE_ANCHOR_CON1 = latency; 
+	BLE_ANCHOR_CON0 = (2<<12)|(HW_ID(hw)<<8)|(1<<2)|1;
+}
+
+static void __set_latency_disable(struct ble_hw *hw)
+{
+	BLE_ANCHOR_CON1 = (hw->state<<12);
+	BLE_ANCHOR_CON0 = (2<<12)|(HW_ID(hw)<<8)|(1<<2)|1;
+}
+
+
 
 static void __set_event_count(struct ble_hw *hw, int count)
 {
@@ -1840,12 +1859,17 @@ static void __hw_tx_process(struct ble_hw *hw)
 	struct ble_tx empty;
 	struct ble_param *ble_fp = &hw->ble_fp;
 
+    static int latency;
+
 	if (hw->state == SLAVE_CONN_ST || hw->state == MASTER_CONN_ST)
 	{
 		if (!hw->rx_ack){
 			rf_putchar('N');
+            latency = __pop_latency(hw);
+            __set_latency_disable(hw);
 			return;
 		}
+        __push_latency(hw, latency);
 
 		i = !(ble_fp->TXTOG & BIT(0));
         /* rf_putchar('0'+i); */
